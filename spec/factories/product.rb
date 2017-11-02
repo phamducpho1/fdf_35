cf9c0b56fc34b97891f34df815161831e1b76081
Hoang Minh <hoangminh11081996@gmail.com>
2017-11-02 08:02:13 +0700

    rspec-view-product

--- spec/factories/product.rb
+++ spec/factories/product.rb
@@ -3,6 +3,7 @@ FactoryGirl.define do
     name {Faker::Name.name}
     price 12000
     quanlity 100
+    image {Rack::Test::UploadedFile.new(File.join(Rails.root, '/app/assets/images/avatar_2x.png'))}
     information {Faker::Lorem.sentence}
     association :category, factory: :category
   end
